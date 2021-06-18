a27fe8fa6763a453748f027c69bbda1bf26f1914
David <d.selioutsky@trameter.com>
2021-06-17 22:09:04 -0400

    Initial Commit

---
+++ Beachbody/BeachbodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachbodyApp.swift
+//  Beachbody
+//
+//  Created by David Selioutsky on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct BeachbodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beachbody/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beachbody
+//
+//  Created by David Selioutsky on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
